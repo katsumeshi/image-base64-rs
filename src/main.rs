680ff60b7ac58e07c5603220a3bc7129a2f6a7a4
Yuki Matsushita <katsumeshi@gmail.com>
2017-02-07 23:31:26 -0800

    modularize function converting to base64

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,42 @@
 extern crate rustc_serialize;
 extern crate regex;
 
-use std::path::Path;
+// use std::path::Path;
 
-use rustc_serialize::base64::{FromBase64, ToBase64, MIME};
-use rustc_serialize::hex::{ToHex};
-use std::fs::File;
-use std::io::Read;
-use std::io::Write;
-use regex::Regex;
+// use std::io::Write;
 use std::str;
+// use std::vec::Drain;
+
+mod image_base64 {
+    use std::fs::File;
+    use rustc_serialize::base64::{ToBase64, MIME};
+    use rustc_serialize::hex::{ToHex};
+    use regex::Regex;
+    use std::io::Read;
+    use std::string::String;
+
+    pub fn to_base64(path: &str) -> String {
+        let mut file = File::open(path).unwrap();
+        let mut v = Vec::new();
+        let _ = file.read_to_end(&mut v);
+        let b64 = v.to_base64(MIME);
+        let hex = v.to_hex();
+        return format!("data:image/{};base64,{}", get_type(&hex), b64);
+    }
+
+    fn get_type(hex: &str) -> &str {
+        if Regex::new(r"^ffd8ffe0").unwrap().is_match(hex) { "jpg" }
+        else if Regex::new(r"^89504e47").unwrap().is_match(hex){ "png" }
+        else if Regex::new(r"^47494638").unwrap().is_match(hex){ "gif" }
+        else { panic!("invalid file") }
+    }
+}
 
 fn main() {
 
     // ------------------------------------------------------
-	let mut file = File::open("res/test.jpg").unwrap();
-    let mut v = Vec::new();
-    let _ = file.read_to_end(&mut v);
 
-    let b64 = v.to_base64(MIME);
-    let hex = v.to_hex();
-    let fmt_base64 = format!("data:image/{};base64,{}", get_type(&hex), b64);
+    let fmt_base64 = image_base64::to_base64("res/test.jpg");
     let offset = fmt_base64.find(',').unwrap_or(fmt_base64.len())+1;
     let mut header = fmt_base64;
     let value: String = header.drain(..offset).collect();
@@ -39,11 +55,4 @@ fn main() {
 	// let img = b64.from_base64().unwrap();
     // let mut file = File::create(&Path::new("test.jpg")).unwrap();
     // file.write_all(img.as_slice());
-}
-
-fn get_type(file: &str) -> &str {
-    if Regex::new(r"^ffd8ffe0").unwrap().is_match(file) { "jpg" }
-    else if Regex::new(r"^89504e47").unwrap().is_match(file){ "png" }
-    else if Regex::new(r"^47494638").unwrap().is_match(file){ "gif" }
-    else { panic!("invalid file") }
 }
