429612976d1f55d8b36c01565722ad6d08f51297
Daniel Sauvé <megametres@users.noreply.github.com>
2020-06-17 21:53:08 -0400

    Remove the Regex dependency + autoformat

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,18 @@
 extern crate rustc_serialize;
-extern crate regex;
 
-use std::fs::File;
 use rustc_serialize::base64::{FromBase64, ToBase64, MIME};
-use rustc_serialize::hex::{ToHex};
-use regex::Regex;
+use rustc_serialize::hex::ToHex;
+use std::fs::File;
 use std::io::Read;
 use std::string::String;
 
 pub fn get_file_type(hex: &str) -> &str {
-    if Regex::new(r"^ffd8ffe0").unwrap().is_match(hex) { 
-        return "jpeg" 
-    } else if Regex::new(r"^89504e47").unwrap().is_match(hex) {  
-        return "png" 
-    } else if Regex::new(r"^47494638").unwrap().is_match(hex) { 
-        return "gif"
-    } 
-    panic!("invalid file type")
+    match &hex[..8] {
+        r"ffd8ffe0" => "jpeg",
+        r"89504e47" => "png",
+        r"47494638" => "gif",
+        _ => panic!("invalid file type"),
+    }
 }
 
 pub fn to_base64(path: &str) -> String {
@@ -25,12 +21,16 @@ pub fn to_base64(path: &str) -> String {
     let _ = file.read_to_end(&mut vec);
     let base64 = vec.to_base64(MIME);
     let hex = vec.to_hex();
-    return format!("data:image/{};base64,{}", get_file_type(&hex), base64.replace("\r\n", ""));
+    return format!(
+        "data:image/{};base64,{}",
+        get_file_type(&hex),
+        base64.replace("\r\n", "")
+    );
 }
 
 pub fn from_base64(base64: String) -> Vec<u8> {
-    let offset = base64.find(',').unwrap_or(base64.len())+1;
+    let offset = base64.find(',').unwrap_or(base64.len()) + 1;
     let mut value = base64;
     value.drain(..offset);
-    return value.from_base64().unwrap();
+    value.from_base64().unwrap()
 }
